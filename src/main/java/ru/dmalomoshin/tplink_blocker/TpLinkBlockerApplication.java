f193182d8052cfb3aeba2d8a43e9ad7cfe4b3e5f
DmitryMalomoshin <makropulas@gmail.com>
2024-05-06 12:18:39 +0300

    Start project

---
+++ src/main/java/ru/dmalomoshin/tplink_blocker/TpLinkBlockerApplication.java
@@ -0,0 +1,13 @@
+package ru.dmalomoshin.tplink_blocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpLinkBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpLinkBlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmalomoshin/tplink_blocker/TpLinkBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmalomoshin.tplink_blocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpLinkBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
